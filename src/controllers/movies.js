1b7d3cd046d9a3f638b154ccfae0e9830340a23d
israelbrea12 <israelbrea.02@gmail.com>
2025-02-17 10:42:03 +0100

    Proyecto terminado V2

--- src/controllers/movies.js
+++ src/controllers/movies.js
@@ -6,14 +6,17 @@ const moviesController = {
         try {
             let { page, limit } = req.query;
 
+            // Convierte `page` y `limit` a números, validando que sean enteros.
             // Solucionado gracias a testing, ya que antes detectaba como string y lo dejaba pasar.
             page = Number.isInteger(Number(page)) ? Number(page) : 1;
             limit = Number.isInteger(Number(limit)) ? Number(limit) : 10;
 
+            // Valida que `page` y `limit` sean mayores a 0
             if (page < 1 || limit < 1) {
                 return res.status(400).json({ error: "Los valores de page y limit deben ser mayores a 0." });
             }
 
+            // Obtiene las películas con paginación
             const moviesData = await MovieModel.findAllMovies(page, limit);
 
             res.json(moviesData);
--- src/models/movie.js
+++ src/models/movie.js
@@ -7,16 +7,17 @@ const MovieModel = {
     // Busca todas las películas con paginación.
     async findAllMovies(page = 1, limit = 10) {
         const offset = (page - 1) * limit;
+        // Obtiene el total de películas y las películas correspondientes a la página solicitada
         const { count, rows } = await this.getModel().findAndCountAll({
             offset,
             limit,
         });
 
         return {
-            results: rows,
-            hasNextPage: offset + rows.length < count,
-            pageSize: rows.length,
-            next: offset + rows.length < count ? `/movies?page=${page + 1}&limit=${limit}` : null,
+            results: rows, // Lista de películas obtenidas
+            hasNextPage: offset + rows.length < count, // Indica si hay más páginas disponibles
+            pageSize: rows.length, // Cantidad de películas en la página actual
+            next: offset + rows.length < count ? `/movies?page=${page + 1}&limit=${limit}` : null, // URL de la siguiente página, si existe
         };
     },
 };
